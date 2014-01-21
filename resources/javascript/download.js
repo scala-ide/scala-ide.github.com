874fbf90411bc0654ff66bfe23c993e98cb7adea
François Garillot <francois@garillot.net>
2014-01-21 15:02:33 +0100

    Removing ZeroClipboard
    Unsubtle removal of the ZeroClipboard flash snippet.
    
    See advisory: http://seclists.org/fulldisclosure/2013/Mar/207
    Thanks a lot to Shahee Mirza ( @shaheemirza ) for reporting this!

--- resources/javascript/download.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-  var clip = new ZeroClipboard.Client();
-  clip.setHandCursor( true );
-  clip.setText("http://download.scala-ide.org/releases-29/stable/site");	  			
-  clip.glue("download-link");
-});
